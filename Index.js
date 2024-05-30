9df5a2bc2998fd439459c8d553af006345c56dec
Arvind83291 <Arvind83291@gmail.com>
2024-05-30 23:59:06 +0530

    update

--- Index.js
+++ Index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3001;
 
+const zod = require("zod"); 
+
+
+const port = 3001;
+app.use(express.json());//this is for reading the inputs from body while send the request
+
+//This is exampe of Query parametre and ugly way of doing Authentication and input validations
 app.get("/health-checkup",function (req, res){
      const kidneyId = req.query.kidneyId;
      const username= req.headers.username;
@@ -22,4 +28,20 @@ app.get("/health-checkup",function (req, res){
     });
 });
 
+// this is the Example of taking the input using the body
+//always use app.use(express.json()) to read the input within the body
+app.post("/kidneylength",function(req,res){
+    const kidneys = req.body.kidneys;
+    const kidneyLength=kidneys.length;
+    res.send("you have "+ kidneyLength+ " Kidneys")
+})
+
+// this is the example of Exception handling midddle ware 
+app.use(function(err,req,res,next){
+    res.json({
+        msg:"Sorry something is Wrong"
+    })
+})
+
+// this is the example of zod where we are 
 app.listen(port);
